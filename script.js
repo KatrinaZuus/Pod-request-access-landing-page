26ea0c67fd5de40781b2ea9ee2488de8eafee060
KatrinaZuus <katrinazuus@gmail.com>
2024-02-02 11:21:24 +0400

    mail validation

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const emailInput = document.getElementById("email");
-console.log (emailInput)
-let emailValue = ''
-emailInput.addEventListener("input", (event) => {
-    console.log(event.target.value);
-});
+const btnSubmit = document.getElementById("btn");
+const error = document.getElementById("error");
+
+
+btnSubmit.addEventListener("click", (evnt) => {
+    evnt.preventDefault()
+    if (!emailInput.value.includes("@") || !emailInput.value.includes(".") || !emailInput.value.includes("")) {
+        error.style.display= "block"
+        emailInput.value = ""
+    } else error.style.display= "none"
+
+
+})
+
+
