0526fdf96176e960d4a9a033b9c5a65fef85c3f6
KatrinaZuus <katrinazuus@gmail.com>
2024-02-16 23:00:48 +0400

    js gavaswore

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const btnSubmit = document.getElementById("btn");
 const error = document.getElementById("error");
 
 
-btnSubmit.addEventListener("click", (evnt) => {
-    evnt.preventDefault()
-    if (!emailInput.value.includes("@") || !emailInput.value.includes(".") || !emailInput.value.includes("")) {
-        error.style.display= "block"
-        emailInput.value = ""
-    } else error.style.display= "none"
-
+btnSubmit.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (emailInput.value.includes("@") && emailInput.value.includes(".")) {
+        error.style.display= "none"
+        
+    }  else if (emailInput.value.includes("")){
+         error.style.display= "block"
+         emailInput.value = ""
+    }
 
 })
 
